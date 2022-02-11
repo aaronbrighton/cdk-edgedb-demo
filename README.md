e2e112fb2dc7a1c53b77b01ad297ee4d053394e7
Aaron Brighton <aaron@aaronbrighton.ca>
2022-02-11 06:40:24 -0500

    Initial commit for a load balanced EdgeDB service in AWS

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# replace this
+# cdk-edgedb-aurora-example
+
+### Prequisites
+
+- Node.JS
+- Yarn
+- AWS CDK
+- AWS Account and AWS CLI Configured
+
+### Quick deployment commands
+
+```
+projen
+yarn deploy
+```
+
+Your generated EdgeDB Server endpoint is provided in the Outputs:
+
+```
+cdk-edgedb-aurora-demo.edgedbendpoint = cdk-e-Servi-2WRTSY4QYNFS-1b1dc6202d12d7eb.elb.us-east-1.amazonaws.com
+```
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,74 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
-export class MyStack extends Stack {
+export class CdkEdgeDBAuroraDemo extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 0,
+      maxAzs: 2,
+    });
+
+    const defaultDatabaseName: string = 'postgres';
+    const cluster = new rds.DatabaseCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_13_4 }),
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MEDIUM),
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        vpc,
+      },
+      defaultDatabaseName,
+    });
+
+    const loadBalancedFargateService = new ecsPatterns.NetworkLoadBalancedFargateService(this, 'Service', {
+      vpc,
+      memoryLimitMiB: 2048,
+      cpu: 1024,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("edgedb/edgedb"),
+        containerPort: 5656,
+        environment: {
+          /*EDGEDB_SERVER_HTTP_ENDPOINT_SECURITY: 'optional',*/
+          EDGEDB_SERVER_TLS_CERT_MODE: 'generate_self_signed',
+          EDGEDB_SERVER_PASSWORD: "SuperS3cretPwd$", // Need to update to use Secrets Manager to auto-generate outside of code.
+          EDGEDB_SERVER_BACKEND_DSN: `postgres://${cluster.secret?.secretValueFromJson('username')}:${cluster.secret?.secretValueFromJson('password')}@${cluster.clusterEndpoint.hostname}:${cluster.secret?.secretValueFromJson('port')}/${defaultDatabaseName}`,
+        },
+      },
+      desiredCount: 1,
+      publicLoadBalancer: true,
+      healthCheckGracePeriod: Duration.minutes(2),
+      minHealthyPercent: 100,
+      maxHealthyPercent: 200,
+      listenerPort: 5656,
+      assignPublicIp: true,
+    });
+    loadBalancedFargateService.service.connections.allowFromAnyIpv4(ec2.Port.tcp(5656));
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: '/server/status/ready',
+      interval: Duration.seconds(10),
+      unhealthyThresholdCount: 2,
+      healthyThresholdCount: 2,
+      protocol: elbv2.Protocol.HTTPS,
+    });
+    loadBalancedFargateService.targetGroup.setAttribute('deregistration_delay.timeout_seconds', '10');
+    cluster.connections.allowDefaultPortFrom(loadBalancedFargateService.service);
+
+    new CfnOutput(this, 'edgedb-endpoint', {
+      value: loadBalancedFargateService.loadBalancer.loadBalancerDnsName,
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
-
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+new CdkEdgeDBAuroraDemo(app, 'cdk-edgedb-aurora-demo');
 
 app.synth();
